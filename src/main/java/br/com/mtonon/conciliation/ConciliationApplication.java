7082675adee11873e2fbc5360bf68ab45fb3fad2
mbettcher <mtonon.pmg@gmail.com>
2021-11-22 17:04:06 -0300

    Projeto Criado

---
+++ src/main/java/br/com/mtonon/conciliation/ConciliationApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.conciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciliationApplication.class, args);
+	}
+
+}
